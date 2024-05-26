6c85eb0ad2c4e80771a30f94c86dd8d75130c7c6
Anand Vardhan <anandvar669@gmail.com>
2024-05-27 02:35:05 +0530

    updating prompt template

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
+        ("{question}")
     ]
 )
 ## streamlit framework
