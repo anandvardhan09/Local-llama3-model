3db1a3c513b63440311ef6e59c7a60e5a5f7656f
Anand Vardhan <anandvar669@gmail.com>
2024-05-27 02:04:57 +0530

    fixing Tracing of langsmith

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
 import streamlit as st
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACKING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
     [
